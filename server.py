bf9ca742e03386ecf2a738375010bbc595c3dfe4
邱振豪 <280690956@qq.com>
2018-09-06 16:24:38 +0800

    服务器修改
    增加了异常信息匹配算法

--- server.py
+++ server.py
@@ -4,8 +4,9 @@ from flask import request
 from moder_bulid import model_build
 from equiment_pre import IsolationForest
 import json
-#from match_model import match_model
+from match_model import match_model
 from flask_api import status
+from match_model import got_train
 app = Flask(__name__)
 
 @app.route('/',methods=['POST'])
@@ -30,12 +31,9 @@ def ac():
         iso = IsolationForest(data)
         names = iso.equipment_pred()
         if len(names) != 0:
-            got_train = False
-            ''''
-            fault_name, fault_sim = match_model(X_test=data,got_train)  # 异常名称和异常相似度
+            fault_name, fault_sim = match_model(X_test=data)  # 异常名称和异常相似度
             dic_message['异常名称'] = fault_name
             dic_message['异常相似度'] = fault_sim
-            '''
             dic_message['异常指标名称'] = names
             dic_message['coder_order'] = code_order
             dic_message['devld'] = devId
