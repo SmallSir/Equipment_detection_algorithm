96a76ced40284747467a28d637807c4d72075028
邱振豪 <280690956@qq.com>
2018-09-07 17:18:54 +0800

    修改了服务器的返回内容
    规定了返回内容为json形式，同时由于不同的错误情况修改了返回状态码

--- server.py
+++ server.py
@@ -12,8 +12,10 @@ app = Flask(__name__)
 @app.route('/health',methods=['POST','GET'])
 def ac():
     if request.method == 'GET':
-        print("'Error':Method Not Allowed,'Method':Only Accept POST Request")
-        return json.dumps(status.HTTP_505_HTTP_VERSION_NOT_SUPPORTED)
+        return json.dumps(dict({
+            'Error': 'Method Not Allowed',
+            'Method': 'Only Accept POST Request'
+        })),405
     datas = json.loads(request.get_data().decode())
     devId = datas['devId']
     code_order= datas['code_order']
@@ -21,27 +23,31 @@ def ac():
     k = -1
     for x in data:
         if len(data[x]) != k and k != -1:
-            print("'Error': Unsupported Media Type,'Input': Please Input the Right Dict Type!")
-            return json.dumps(status.HTTP_405_METHOD_NOT_ALLOWED05_HTTP_VERSION_NOT_SUPPORTED)
+            return json.dumps(dict({
+                'Error': 'Unsupported Media Type',
+                'Input': 'Please Input the Right Dict Type!'
+            })),505
         if k == -1:
             k = len(data[x])
     dict_message = {}
     if k != 1:
         model = model_build(data)
         model.model_building()
-        return json.dumps(200)
+        return dict({'Outcome': 'The Algorithm Training is Finished Successfully!'}),200
     else:
         dic_message = {}
         iso = IsolationForest(data)
         names = iso.equipment_pred()
         if len(names) != 0:
+            '''
             fault_name, fault_sim = match_model(X_test=data)  # 异常名称和异常相似度
             dic_message['fault_name'] = fault_name
             dic_message['fault_same'] = fault_sim
+            '''
             dic_message['fault_index_name'] = names
         dic_message['coder_order'] = code_order
         dic_message['devld'] = devId
-        return json.dumps(dic_message)
+        return json.dumps(dic_message),200
 if __name__ == '__main__':
     app.debug = True
     app.run(host='192.168.0.48',port=8080)
