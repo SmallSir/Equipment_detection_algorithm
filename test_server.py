ce506f6da8220492e9b41f1a208c85af1d2b4cf7
邱振豪 <280690956@qq.com>
2018-09-11 16:49:38 +0800

    测试数据修改

--- test_server.py
+++ test_server.py
@@ -1,22 +1,21 @@
 # -*- coding: utf-8 -*-
 import requests
 import json
+import demjson
 url = 'http://192.168.0.48:8080/health'
-dict = {"health":{
+dictss = {"health":{
          "cpu":[0.81,0.66,0.24,0.34,0.15],
          "disk":[0.81,0.66,0.24,0.34,0.15],
          "storage":[0.81,0.66,0.24,0.34,0.15]},
  "devId":1,
  "code_order":1}
-dict = {"health":{
-         "cpu":[34,45],
+dicts = {"health":{
+         "cpu":[34],
          "disk":[34],
          "storage":[34]},
  "devId":1,
 
  "code_order":1
 }
-test = json.dumps(dict)
-r= requests.post(url,data = json.dumps(dict))
-
+r= requests.post(url,data = json.dumps(dicts))
 print(r.json())
